0426880c22f21d1038290f7b26ce462fbb9c66f4
engineerJPark <jshacist@gmail.com>
2025-05-18 03:33:00 +0900

    batch size to 4

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -358,7 +358,7 @@ class TrainConfig:
     # Random seed that will be used by random generators during training.
     seed: int = 42
     # Global batch size.
-    batch_size: int = 8 # 32
+    batch_size: int = 4 # 32
     # Number of workers to use for the data loader. Increasing this number will speed up data loading but
     # will increase memory and CPU usage.
     num_workers: int = 2
